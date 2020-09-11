8ad662d5803a54f0fac74f0274ccb79ddfa9ae84
Tayyab Tahir <tayyabtahir@knowledgeplatform.com.pk>
2020-09-11 18:08:17 +0500

    all type jobs

--- Hangfire/Startup.cs
+++ Hangfire/Startup.cs
@@ -59,7 +59,34 @@ namespace Hangfire
 
 
             app.UseHangfireDashboard();
+
+            //Fire  and forget .Fire - and - forget jobs are executed only once and almost immediately after creation.
             backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+            //Delayed Job
+            var jobId = BackgroundJob.Schedule(() => Console.WriteLine("Delayed!"), TimeSpan.FromSeconds(10));
+
+            //Recurring Job  .Recurring jobs fire many times on the specified CRON schedule.
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("Recurring job fire after every minute, you can change from cron.[any given method name]!"), Cron.Minutely);
+
+
+            // Continuations Job . Continuations are executed when its parent job has been finished.
+            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine("Continuation!"));
+
+            //Batches Pro Batch is a group of background jobs that is created atomically and considered as a single entity.
+
+            //var batchId = BatchJob.StartNew(x =>
+            //{
+            //    x.Enqueue(() => Console.WriteLine("Job 1"));
+            //    x.Enqueue(() => Console.WriteLine("Job 2"));
+            //});
+
+
+            ////Batch Continuations Pro Batch continuation is fired when all background jobs in a parent batch finished.
+
+            //BatchJob.ContinueBatchWith(batchId, x =>
+            //{
+            //    x.Enqueue(() => Console.WriteLine("Last Job"));
+            //});
 
             // ...other configuration logic
             app.UseStaticFiles();
